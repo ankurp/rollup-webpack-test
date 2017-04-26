817efb324e9018c4208f50d4cbf1598a5c48c78b
Ankur Patel <ankur.patel@ymail.com>
2017-04-26 09:26:58 -0400

    Updating example

--- app/math.js
+++ app/math.js
@@ -1,12 +1,11 @@
-
 function getSquare() {
-  console.log('square function');
   return x => x * x;
 }
 
 export const square = getSquare();
 
 export const cube = (x) => {
-  console.log('cube function');
   return x * x * x;
 }
+
+cube(3);
--- build/rollup.js
+++ build/rollup.js
@@ -1,1 +1,1 @@
-!function(){"use strict";function n(){return console.log("square function"),function(n){return n*n}}var o=(n(),function(){return 1});console.log(o())}();
+!function(){"use strict";var n=function(){return 1};console.log(n())}();
